414807f2d2bb332d55b3b97cc297dcb7c159e7b3
Claudio José <claudiojbf11@gmail.com>
2023-04-25 14:55:00 -0300

    Adicionado Validações de Order e OrderItem

--- Store.Domain/Entities/Order.cs
+++ Store.Domain/Entities/Order.cs
@@ -1,3 +1,5 @@
+using Flunt.Notifications;
+using Flunt.Validations;
 using Store.Domain.Enums;
 
 namespace Store.Domain.Entities
@@ -6,6 +8,11 @@ namespace Store.Domain.Entities
     {
         public Order(Customer customer, decimal deliveryFee, Discount discount)
         {
+            AddNotifications(new Contract<Notification>()
+                .Requires()
+                .IsNotNull(customer, "Customer", "Cliente Invalido")
+            );
+
             Customer = customer;
             Date = DateTime.Now;
             Number = Guid.NewGuid().ToString().Substring(0, 8);
@@ -26,7 +33,8 @@ namespace Store.Domain.Entities
         public void AddItem(Product product, int quantity)
         {
             var item = new OrderItem(product, quantity);
-            Items.Add(item);
+            if(item.IsValid)
+                Items.Add(item);
         }
 
         public decimal Total()
